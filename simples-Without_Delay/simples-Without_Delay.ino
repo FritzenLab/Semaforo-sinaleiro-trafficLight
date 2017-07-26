b162943f61c08cb8c0e3950045713fcda4fd5b4d
clovisf <clovisf@gmail.com>
2017-07-26 00:23:16 -0300

    semaforo traffic light withtout delay arduino
    semaforo sinaleiro traffic light withtout delay arduino

---
+++ simples-Without_Delay/simples-Without_Delay.ino
@@ -0,0 +1,77 @@
+#define luzVermelha 2
+#define luzAmarela 3
+#define luzVerde 4
+
+int tickDoRelogio = 0;
+int tempoAmarelo = 5;
+int tempoVermelho = 6;
+int tempoReinicia = 15;
+
+
+// Variables used for timing
+unsigned long timet;
+unsigned long tempoativo1inicio;
+unsigned long tempoativo1final;
+unsigned long tempototal1;
+unsigned long previousTime;
+boolean enterFunction = true;
+//-----------------------
+
+
+void setup() {
+  // set the digital pin as output:
+  pinMode(luzVermelha, OUTPUT);
+  pinMode(luzAmarela, OUTPUT);
+  pinMode(luzVerde, OUTPUT);
+}
+
+void loop() {
+
+   timet = micros();
+  if (enterFunction == true){
+    previousTime= timet;
+    // Write your first code below 
+    //-----------------------
+    tickDoRelogio = tickDoRelogio + 1;
+    if (tickDoRelogio <= tempoAmarelo){
+      ligaVerde();
+    } else if ((tickDoRelogio >= tempoAmarelo) && (tickDoRelogio <= tempoVermelho)){
+      ligaAmarelo();       
+    } else if ((tickDoRelogio > tempoVermelho) && (tickDoRelogio <= tempoReinicia)){
+      ligaVermelho();
+    } else{
+      tickDoRelogio = 0;
+    }
+    
+    //-----------------------
+    // End of your first code
+  }  
+   
+
+  // The DELAY time is adjusted in the constant below >> 
+  if (timet - previousTime < 999990){ // 1 million microsencods= 1 second delay
+    /* I have actually used 0.999990 seconds, in a trial to compensate the time that
+       this IF function takes to be executed. this is really a point that
+       need improvement in my code */   
+    enterFunction= false;
+  } 
+  else {
+    enterFunction= true;
+  }
+}
+int ligaVerde(){
+  digitalWrite(luzVerde, HIGH);
+  digitalWrite(luzAmarela, LOW);
+  digitalWrite(luzVermelha, LOW);
+}
+int ligaAmarelo(){
+  digitalWrite(luzVerde, LOW);
+  digitalWrite(luzAmarela, HIGH);
+  digitalWrite(luzVermelha, LOW);  
+}
+int ligaVermelho(){
+  digitalWrite(luzVerde, LOW);
+  digitalWrite(luzAmarela, LOW);
+  digitalWrite(luzVermelha, HIGH);
+}
+
